aeb5e6846a0cbb452b7001fa571b7bf718d29200
kokookok77 <50436240+kokookok77@users.noreply.github.com>
2020-10-04 18:01:42 +0900

    route.py app.py에 추가

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, Blueprint, request
 from flask import jsonify, make_response, redirect, url_for
 from werkzeug.utils import secure_filename
+from routes import bp as image_bp
 
 from python_code.utils import allowed_file
 
@@ -10,7 +11,7 @@ import config
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-
+app.register_blueprint(image_bp)
 
 @app.route("/profile")
 @app.route("/")
