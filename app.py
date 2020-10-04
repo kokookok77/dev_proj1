e39491d4d6d3e6de5ec6a1cfa13b5c2b8d30a2ad
kokookok77 <50436240+kokookok77@users.noreply.github.com>
2020-10-04 17:55:19 +0900

    routes.py 원복 및 file upload 코드 리팩토링

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from python_code.utils import allowed_file
 import config
 
 app = Flask(__name__)
-
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
@@ -17,33 +16,6 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/")
 def profile():
     return render_template("profile.html")
-
-
-@app.route('/imageUpload', methods=["POST"])
-def imageUpload():
-    '''
-        Image Upload
-    '''
-
-    try:
-        files = request.files.getlist("uploadImage")
-        print(files)
-        for file in files:
-            print(file)
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(config.img_save_path, filename))
-
-        return
-        # if file and allowed_file(file.filename):
-        #     file.save(os.path.join('./', filename))
-        #     print(file.filename)
-        # else:
-        #     print('Error: "File type is not allowed"')
-
-    except Exception as e:
-        print('Error:', e)
-
-        return ""
 
 
 if __name__ == "__main__":
